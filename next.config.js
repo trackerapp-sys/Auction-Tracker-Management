717a1d3bb0deac8c6be63aaa1c63d5330ce8129a
trackerapp-sys <ymwalker1978@gmail.com>
2025-09-29 14:01:28 +1000

    Configure Next.js to output to apps/web/dist for Render compatibility

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove experimental.appDir as it's now stable in Next.js 14
+  distDir: 'apps/web/dist',
 }
 
 module.exports = nextConfig
